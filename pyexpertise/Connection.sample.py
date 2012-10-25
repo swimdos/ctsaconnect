7b8a932d8a3e41409c33a98506b46d980eabddac
Carlo Tornia <carlotorniai@gmail.com>
2012-10-25 09:14:32 -0700

    Added generateProvierSummary to generate the CSV files.
    Signed-off-by: Carlo Tornia <carlotorniai@gmail.com>

---
+++ pyexpertise/Connection.sample.py
@@ -0,0 +1,5 @@
+host='your_host'
+port=3306
+user = 'your_user'
+password = 'your_password#71'
+database= 'your_db'
---
+++ pyexpertise/generateProviderSummary.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+
+#Insert BSD License (http://opensource.org/licenses/BSD-2-Clause)
+
+##############################################################################
+# Copyright (c) 2012, Carlo Torniai ( carlotonriai at gmail.com )
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+# THE POSSIBILITY OF SUCH DAMAGE.
+##############################################################################
+
+import MySQLdb
+import Connection
+import csv
+
+##############################################################################
+# Summary:
+# This module executes a stored procedure to generate  csv files to be used
+# by expertise.py and other scripts.
+# It requires that a COnnection.py exists (See Connection.sample.py for
+#
+# it requires to have installed the MySQLdb module -> easy_install MySQL-python
+# it currently uses the database schema for OHSU ICD9 data
+# but the queries can be adapted to other schemas
+##############################################################################
+
+
+
+##############################################################################
+# Function that writes a csv in filename using the value returned by
+# executeGetICDCountSQL
+##############################################################################
+def writeCSV(entry, filename):
+    csvfile = open(filename, 'wb')
+    spamwriter = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(icd_entry)
+
+##############################################################################
+# function that returns a DB connection
+##############################################################################
+def getDB(host, port,  user, password, database):
+    db = MySQLdb.connect(host=host, port=port, user=user, passwd=password, db=database)
+    return db
+
+
+##############################################################################
+# Execute the query to get the data for
+##############################################################################
+def executeGetICDCountSQL(npi):
+    queryresults = []
+    db = getDB(Connection.host, Connection.port, Connection.user, Connection.password, Connection.database)
+    sql = "SELECT  ctsadata.icd_simple.icd, icd9_code.term, COUNT(ctsadata.icd_simple.icd) AS Occurrence \
+FROM ctsadata.icd_simple , icd9_code \
+WHERE ctsadata.icd_simple.npi='%s' AND icd9_code.code = ctsadata.icd_simple.icd  \
+GROUP BY ctsadata.icd_simple.icd \
+ORDER BY Occurrence DESC" % (npi)
+# Run query and get result
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    except Exception, e:
+        print e
+    # Loop through result
+    for row in result:
+        queryresults.append(row)
+
+    # Close the Connection
+    db.close()
+    return queryresults
+
+
+##############################################################################
+# Execute the stored Procedure getICDCount that return all the unique ID
+# for providers the source for the query can be found in getUniqueProviders.sql
+# under ./queries
+##############################################################################
+def getUniqueProviders():
+    providers_npi=[]
+    db = getDB(Connection.host, Connection.port, Connection.user, Connection.password, Connection.database)
+    sql = "call getUniqueProviders()"
+
+    # Run query and get result
+    cursor = db.cursor()
+    try:
+        print 'Calling getICDCount Stroe Procedure'
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    except Exception, e:
+        print e
+    # Loop through result
+    for row in result:
+        providers_npi.append(row)
+
+    return providers_npi
+
+
+def main():
+    print 'Starting execution'
+    providers = getUniqueProviders()
+    print providers
+    for provider_npi in providers:
+        print 'Getting data for provider %s' % (provider_npi)
+        filename="./results/%s.xls" % (provider_npi)
+        icd_results = executeGetICDCountSQL(provider_npi)
+        for icd_entry in icd_results:
+            writeCSV(provider_npi, filename)
+    print 'Done'
+
+if __name__ == '__main__':
+    main()
